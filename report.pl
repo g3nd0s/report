9f2c08b38347a0427d257d05227b9dc5ffb777c6
g3nd0s <g3nd0s@gmail.com>
2012-12-19 01:31:19 +0200

    Update report.pl

--- report.pl
+++ report.pl
@@ -6,31 +6,23 @@ my @args;
 @args = @ARGV;
 ###############
 sub get_content_from_file {
-  my ($file) = @_;
-  my $content;
+  my ( $file, $content ) = shift;
   open(FILE,"$file") or die "$!\n";
-  while (<FILE>) {
-    $content.="$_";
-  }
+  $content .= $_ while <FILE>;
   close(FILE);
   return $content;
 }
 sub oshidanie {
-  my ($ref_array) = @_;
-  my $temp;
-  map {$temp+=$_;} @{$ref_array};
+  my ( $ref_array, $temp ) = @_;
+  $temp += $_ foreach @$ref_array;
   my $average = $temp/(($#{$ref_array}+1)*60);
-  undef $temp;
-  my ($hour,$ostatok) = ($average =~ /(\d+)\.(\d+)/);
+  my ( $hour, $ostatok ) = ($average =~ /(\d+)\.(\d+)/);
   $temp = "0.$ostatok"*60;
   my $min = ($temp =~/(\d*)\./)[0];
-  undef $temp;
   my $oshidanie = join(":",$hour,$min);
   return $oshidanie;
 }
 main: {
-  my @array;
-  map {push(@array,$_)} 400..450;
-  my $ref = \ @array;
-  oshidanie($ref);
+  my $range = [400..450];
+  print oshidanie( $range );
 }
